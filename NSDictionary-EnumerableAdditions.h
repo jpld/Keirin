261e9ede679b343db3cf6bb2bc645a82fc290a60
jpld <imjunpyo@gmail.com>
2009-10-11 22:44:27 -0700

    add skeleton files for NSDictionary

---
+++ NSDictionary-EnumerableAdditions.h
@@ -0,0 +1,12 @@
+//
+//  NSDictionary-EnumerableAdditions.h
+//  Keirin
+//
+//  Created by jpld on 11 Oct 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSDictionary(Enumerable)
+@end
---
+++ NSDictionary-EnumerableAdditions.m
@@ -0,0 +1,12 @@
+//
+//  NSDictionary-EnumerableAdditions.m
+//  Keirin
+//
+//  Created by jpld on 11 Oct 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSDictionary-EnumerableAdditions.h"
+
+@implementation NSDictionary(Enumerable)
+@end
