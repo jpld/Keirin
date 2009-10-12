517d7f4e28344e1f02c3431d5907686e70b33360
jpld <imjunpyo@gmail.com>
2009-10-11 20:48:13 -0700

    change date format

--- KeirinAppDelegate.h
+++ KeirinAppDelegate.h
@@ -2,7 +2,7 @@
 //  KeirinAppDelegate.h
 //  Keirin
 //
-//  Created by jpld on 11 october 2009.
+//  Created by jpld on 11 Oct 2009.
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
--- main.m
+++ main.m
@@ -2,13 +2,12 @@
 //  main.m
 //  Keirin
 //
-//  Created by jpld on 11 october 2009.
+//  Created by jpld on 11 Oct 2009.
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     return NSApplicationMain(argc,  (const char **) argv);
 }
