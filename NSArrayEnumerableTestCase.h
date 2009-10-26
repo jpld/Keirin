8b00227569428fa7091563a523e258f08b4a0bf6
jpld <imjunpyo@gmail.com>
2009-10-26 00:28:28 -0700

    add unit test target to project and empty NSArray test case

---
+++ NSArrayEnumerableTestCase.h
@@ -0,0 +1,12 @@
+//
+//  NSArrayEnumerableTestCase.h
+//  Keirin
+//
+//  Created by jpld on 26 Oct 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSArrayEnumerableTestCase : SenTestCase {}
+@end
---
+++ NSArrayEnumerableTestCase.m
@@ -0,0 +1,12 @@
+//
+//  NSArrayEnumerableTestCase.m
+//  Keirin
+//
+//  Created by jpld on 26 Oct 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSArrayEnumerableTestCase.h"
+
+@implementation NSArrayEnumerableTestCase
+@end
